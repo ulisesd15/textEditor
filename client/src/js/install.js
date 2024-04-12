34be05ff23526ea0de04bb5a83ab3d9566a96b95
ulisesd404 <144632800+ulisesd404@users.noreply.github.com>
2024-04-12 11:09:57 -0700

    ulidev

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -2,10 +2,31 @@ const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+
+window.addEventListener('beforeinstallprompt', (event) => {
+    window.defferedPrompt = event;
+    // Show the install button
+    butInstall.classList.toggle('hidden', false);
+});
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async () => {
+    const promptEvent = window.defferedPrompt;
+
+    if (!promptEvent) {
+        return;
+    }
+
+    //show prompt
+    promptEvent.prompt();
+
+    //reset the deferred prompt variable
+    window.defferedPrompt = null;
+
+    butInstall.classList.toggle('hidden', true);
+});
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => {
+    window.defferedPrompt = null;
+});
